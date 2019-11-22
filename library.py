1c792c5b30f6d65b1f9efe3f0ec548bf951084d1
Gianluca <gianluca.ghinazzi@gmail.com>
2019-11-22 10:22:47 +0100

    Library creation

---
+++ library.py
@@ -0,0 +1,17 @@
+def TrasformStringInt:
+	while ok == len(a):
+	number= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", " "]
+	ok=0
+	for i in range(len(a)):
+		if a[i] in Number:
+			ok += 1
+		else:
+			ok -=1
+	if ok < len(a):
+		print("La variabile inserita non è numerica:")
+		a = input("Reinserire una variabile: ")
+		ok = len(a)
+	elif ok == len(a):
+		c = float(a)
+		print("La variabile inserita corrisponde a ", c)
+		ok = len(a)-1
